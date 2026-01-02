97ff19a590fe391f86289219a13be6b7ce10bdcf
Noufal <noufalkakdlr@gmail.com>
2026-01-02 17:58:54 +0530

    Auto-backup: 2026-01-02 17:58:54

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,12 +3,22 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
+    opts = {
+      colors = {
+        bg = "#161719",
+      },
+      highlights = {
+        Normal = { bg = "#161719" },
+        NormalFloat = { bg = "#161719" },
+        NormalNC = { bg = "#161719" },
+      },
+    },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_vivid",
+      colorscheme = "onedark",
     },
   },
 }
