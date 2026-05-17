1f514999bf85d160646bdfd416355c285dd98f52
Noufal <noufalkakdlr@gmail.com>
2026-05-17 10:51:06 +0530

    Auto-backup: 2026-05-17 10:51:06

---
+++ unstow.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "🧹 Unstowing dotfiles..."
+for folder in */; do
+  folder=${folder%/}
+
+  # Skip specific folders using case
+  case "$folder" in
+  .git | wallpaper)
+    echo "⏭️  Skipping $folder..."
+    continue
+    ;;
+  esac
+
+  echo "🗑️ Removing links for $folder..."
+  stow -D "$folder"
+done
+
+echo "✅ All dotfiles unstowed successfully! സിസ്റ്റം ക്ലീൻ ആയി."
