6233ee30415051f54ce089f598e26604930a0469
Noufal <noufalkakdlr@gmail.com>
2026-09-26 14:57:54 +0530

    Auto-backup: 2026-09-26 14:57:54

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -21,7 +21,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("mako")
 	hl.exec_cmd("waybar")
 	hl.exec_cmd("kdeconnectd")
-	hl.exec_cmd("nwg-panel -c controls -d")
+	hl.exec_cmd("nwg-panel -c controls")
 
 	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
